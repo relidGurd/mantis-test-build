5c8902997ac54181cb7d4aae2986799f3ddb7ae8
unknown <titarevdeniz@yandex.ru>
2025-07-04 15:22:35 +0300

    fiz

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,6 +3,38 @@ import styles from "./page.module.css";
 import SinglePrdouct from "@/pages/single-product/single-product";
 import { getProduct } from "@/api/products/products";
 import qs from "qs";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  const CasesQwery = qs.stringify({
+    populate: {
+      preview_image: {
+        populate: "*",
+      },
+    },
+  });
+  // fetch post information
+  const { data } = await getProduct(slug, CasesQwery);
+
+  return {
+    title: data.title,
+    description: data.description,
+    openGraph: {
+      images: [`https://cms.mantis.ru${data.preview_image.url}`],
+    },
+  };
+}
+
 const ProductPage = async ({
   params,
 }: {
