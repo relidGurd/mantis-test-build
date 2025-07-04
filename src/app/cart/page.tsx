2f2dfdb9f8deb4bd33a312cda89214b21585207d
unknown <titarevdeniz@yandex.ru>
2025-07-04 17:55:02 +0300

    fiz

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,17 +3,31 @@ import styles from "./page.module.css";
 import IntrestingProducts from "@/sections/intresting-products/intresting-products";
 import Breadcrumbs from "@/components/breadcrumbs/breadcrumbs";
 import routes from "@/utils/breadcrumbs-massive";
+import qs from "qs";
+import { getRelatedProducts } from "@/api/products/products";
+const CartPage = async () => {
+  const relatedQwery = qs.stringify({
+    populate: {
+      products: {
+        populate: {
+          preview_image: true,
+        },
+      },
+    },
+  });
 
-const CartPage = () => {
+  const { data: relatedProducts } = await getRelatedProducts(relatedQwery);
+
   return (
     <div>
       <Breadcrumbs className="main-container" list={routes.cartList} />
       <Cart />
-      {/* <IntrestingProducts
+      <IntrestingProducts
+        product_list={relatedProducts.products}
         title={"Вас может заинтересовать"}
         slidesView={4}
         className={"swiper-container"}
-      /> */}
+      />
     </div>
   );
 };
