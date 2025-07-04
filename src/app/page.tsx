d6d674131f11a9e00b22d9a0780d5fdc68f999af
unknown <titarevdeniz@yandex.ru>
2025-07-04 12:23:43 +0300

    fiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import { Logo } from "@/icons/icons";
 import styles from "./page.module.css";
 import MainPage from "@/pages/main-page/main-page";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Mantis - Купить 1С в Мелитополе, Бердянске, ДНР, ЛНР, и Крыму! Заказать на сайте.",
+  description:
+    "У нас можно купить 1С под ключ в Запорожской области, Херсонской области и Крыму.",
+  openGraph: {
+    images: "/Cover.jpg",
+  },
+};
 
 export default async function Home() {
   return (
