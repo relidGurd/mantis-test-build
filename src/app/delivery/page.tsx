6baccad00e9baa162406fef2ae73257c323a5f97
unknown <titarevdeniz@yandex.ru>
2025-07-10 11:27:55 +0300

    fix

---
+++ src/app/delivery/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import classNames from "classnames";
+import styles from "./delivery.module.css";
+import Typography from "@/ui-kit/typography/typography";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mantis | Условия доставки",
+  description:
+    "У нас можно купить 1С под ключ в Запорожской области, Херсонской области и Крыму.",
+};
+
+const Delivery = () => {
+  return (
+    <div className={classNames("main-container")}>
+      <Typography
+        className={styles.page_title}
+        variant="h1"
+        outline="bold"
+        register="40"
+      >
+        Доставка
+      </Typography>
+      <div className={styles.delivery_container}>
+        <div className={styles.delivery_list}>
+          <div>
+            <Typography variant="h3" outline="bold" register="32">
+              1. Условия доставки
+            </Typography>
+            <Typography className={styles.deliver_info} variant="p">
+              Мы — IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования. Наша
+              цель — обеспечить высокотехнологичную поддержку вашего бизнеса,
+              от автоматизации процессов до создания web-решений.Мы —
+              IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования.
+            </Typography>
+          </div>
+
+          <div>
+            <Typography variant="h3" outline="bold" register="32">
+              2. Условия доставки
+            </Typography>
+            <Typography className={styles.deliver_info} variant="p">
+              Мы — IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования. Наша
+              цель — обеспечить высокотехнологичную поддержку вашего бизнеса,
+              от автоматизации процессов до создания web-решений.Мы —
+              IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования.
+            </Typography>
+          </div>
+
+          <div>
+            <Typography variant="h3" outline="bold" register="32">
+              3. Условия доставки
+            </Typography>
+            <Typography className={styles.deliver_info} variant="p">
+              Мы — IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования. Наша
+              цель — обеспечить высокотехнологичную поддержку вашего бизнеса,
+              от автоматизации процессов до создания web-решений.Мы —
+              IT-компания, специализирующаяся на комплексных решениях
+              для бизнеса и госсектора. Партнёры ведущих разработчиков
+              программного обеспечения и производителей оборудования.
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Delivery;
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -2,15 +2,43 @@ import classNames from "classnames";
 import styles from "./order.module.css";
 import Typography from "@/ui-kit/typography/typography";
 import OrderForm from "@/components/order-form/order-form";
+import IntrestingProducts from "@/sections/intresting-products/intresting-products";
+import qs from "qs";
+import { getRelatedProducts } from "@/api/products/products";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mantis | Оформление заказа",
+  description:
+    "У нас можно купить 1С под ключ в Запорожской области, Херсонской области и Крыму.",
+};
 
 export default async function Order() {
-  return (
-    <div className={classNames("main-container")}>
-      <Typography outline="bold" register="40" variant="h1">
-        Оформление заказа
-      </Typography>
+  const relatedQwery = qs.stringify({
+    populate: {
+      products: {
+        populate: {
+          preview_image: true,
+        },
+      },
+    },
+  });
+  const { data: relatedProducts } = await getRelatedProducts(relatedQwery);
 
-      <OrderForm />
-    </div>
+  return (
+    <>
+      <div className={classNames("main-container")}>
+        <Typography outline="bold" register="40" variant="h1">
+          Оформление заказа
+        </Typography>
+        <OrderForm />
+      </div>
+      <IntrestingProducts
+        className={"swiper-container"}
+        product_list={relatedProducts.products}
+        title="Вас может заинтересовать"
+      />
+    </>
   );
 }
