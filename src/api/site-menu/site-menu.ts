9df9f68487f79a7e910c99819c99a2fdf76ae44a
unknown <titarevdeniz@yandex.ru>
2025-07-24 18:10:57 +0300

    fix

--- src/api/site-menu/site-menu.ts
+++ src/api/site-menu/site-menu.ts
@@ -13,7 +13,7 @@ async function getSiteMenu(qwery: string) {
     const result = await response.json();
     return result;
   } catch (error: any) {
-    console.error("Ошибка при получении advantages:", error.message);
+    console.error("Ошибка при получении header-menu:", error.message);
     return null;
   }
 }
