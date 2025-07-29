9bc88f96b8483840465309468c16bbc2ac13c85b
unknown <titarevdeniz@yandex.ru>
2025-07-29 16:22:29 +0300

    fiz

--- src/animation/rise-up-animation/animation.ts
+++ src/animation/rise-up-animation/animation.ts
@@ -4,7 +4,7 @@ export const ANIMATION_SETTINGS = {
     delay: 0.15,
     ease: [0.215, 0.61, 0.355, 1],
   },
-  viewport: { amount: 0.5, once: true },
+  viewport: { amount: 0.3, once: true },
 } as const;
 
 export const PRELOADER_DURATION_S = 4;
