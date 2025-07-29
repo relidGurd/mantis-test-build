98a472e69c67f1b52e151082bdeb4bcbe56e82c6
unknown <titarevdeniz@yandex.ru>
2025-07-29 13:31:04 +0300

    fiz

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,27 @@
 import RiseUpAnimation from "@/animation/rise-up-animation/rise-up-animation";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <section className="main-container">
+    <section className="main-container-blog">
       <div className={`error_container_grid`}>
-        <h1 style={{ color: "red" }}>Попробуйте позже</h1>
+        <div className="error_text_info">
+          <h1 className="">Перезагрузите страницу или попробуйте позже</h1>
+          <Link className="err_back" href={"/"}>
+            На главную
+          </Link>
+        </div>
+
         <RiseUpAnimation>
-          <div>
-            <Image alt="404" width={800} height={600} src={"/404.png"} />
+          <div className="err-image-container">
+            <Image
+              className="err-image"
+              alt="404"
+              width={800}
+              height={600}
+              src={"/404.png"}
+            />
           </div>
         </RiseUpAnimation>
       </div>
